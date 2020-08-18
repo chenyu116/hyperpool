114bc378f65228ddcffe710aa97ae9cf04696f37
roger <cheny.roger@gmail.com>
2020-08-18 15:20:48 +0800

    change NewPool

--- pool.go
+++ pool.go
@@ -1,24 +1,23 @@
 package hyperpool
 
 import (
+	"sync"
 	"sync/atomic"
-	"time"
 )
 
-// every queue has 8 cache slots
-var cacheQueueSize uint32
-
 type Pool struct {
-	New        func() interface{}
-	Close      func(i interface{})
-	resetAfter time.Duration
-	lastPut    uint32
-	free       int64
-	shared     poolChain
+	New   func() interface{}
+	pool  sync.Pool
+	gets  int32
+	Limit int32
 }
 
 func (p *Pool) Get() (x interface{}) {
-	x, _ = p.shared.popHead()
+	x = p.pool.Get()
+	if p.Limit == 0 || (x == nil && p.New != nil && p.gets < p.Limit) {
+		x = p.New()
+		atomic.AddInt32(&p.gets, 1)
+	}
 	return
 }
 
@@ -26,65 +25,6 @@ func (p *Pool) Put(x interface{}) {
 	if x == nil {
 		return
 	}
-	p.shared.pushHead(x)
-	// check if still using the pool
-	atomic.StoreUint32(&p.lastPut, 1)
-}
-
-func (p *Pool) Len() int64 {
-	return p.free
-}
-func (p *Pool) reset() {
-	resetTicker := time.NewTicker(p.resetAfter)
-	checkTimes := 0
-	checkLimit := 3
-	resetting := false
-	for range resetTicker.C {
-		if p.lastPut == 1 {
-			atomic.StoreUint32(&p.lastPut, 0)
-			continue
-		}
-		if resetting {
-			continue
-		}
-		if p.lastPut == 0 {
-			if checkTimes < checkLimit {
-				checkTimes++
-				continue
-			}
-			resetting = true
-			for {
-				x := p.Get()
-				if x != nil && p.Close != nil {
-					p.Close(x)
-					continue
-				}
-				if x == nil {
-					break
-				}
-			}
-			for i := 0; i < int(cacheQueueSize*initSize); i++ {
-				p.Put(p.New())
-			}
-			checkTimes = 0
-			resetting = false
-		}
-	}
-}
-
-func NewPool(queueSize uint32, resetAfter time.Duration, n func() interface{}) *Pool {
-	p := new(Pool)
-	p.resetAfter = resetAfter
-	if p.resetAfter == 0 {
-		p.resetAfter = time.Second * 120
-	}
-	cacheQueueSize = queueSize
-
-	for i := 0; i < int(cacheQueueSize*8); i++ {
-		p.Put(n())
-	}
-	p.New = n
-	go p.reset()
-
-	return p
+	p.pool.Put(x)
+	atomic.AddInt32(&p.gets, -1)
 }
--- pool_test.go
+++ pool_test.go
@@ -2,29 +2,21 @@ package hyperpool
 
 import (
 	"testing"
-	"time"
 )
 
-//func BenchmarkPool(b *testing.B) {
-//	var h = NewPool(10, time.Second*3, func() interface{} {
-//		return 1
-//	})
-//
-//	b.RunParallel(func(pb *testing.PB) {
-//		for pb.Next() {
-//			e := h.Get()
-//			_ = e
-//			h.Put(e)
-//		}
-//	})
-//	b.Log(h.Len())
-//}
+func BenchmarkPool(b *testing.B) {
+	h:=&Pool{
+		New:   func() interface{} {
+			return 1
+		},
+		Limit: 2000,
+	}
 
-func TestNewPool(t *testing.T) {
-	h := NewPool(10, time.Second*3, func() interface{} {
-		return 1
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			e := h.Get()
+			_ = e
+			h.Put(e)
+		}
 	})
-	t.Log(h)
-	time.Sleep(time.Second * 15)
-	t.Error(h.Len())
 }
